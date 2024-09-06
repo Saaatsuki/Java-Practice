b4e2f82b160dbd1db9618b9e136584693a032ef7
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2024-09-06 18:55:18 +0900

    240906

---
+++ School/2409/240906/2/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        // 変数の宣言と初期化
+        int bar = 3;
+        float foo = 3.0f;
+        String pos = "Hello";
+        boolean kin = true;
+
+        // 変数を出力
+        System.out.println("int bar = " + bar);
+        System.out.println("float foo = " + foo);
+        System.out.println("String pos = " + pos);
+        System.out.println("boolean kin = " + kin);
+    }
+}
